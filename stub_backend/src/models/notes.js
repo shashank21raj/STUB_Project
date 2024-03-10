d251da64f10451656730f508be31a5b170b4daa1
Shashank <rajshashank2104@gmail.com>
2024-03-11 00:06:31 +0530

    Changed required to false

--- stub_backend/src/models/notes.js
+++ stub_backend/src/models/notes.js
@@ -2,7 +2,7 @@ import mongoose, { model } from "mongoose";
 const { Schema } = mongoose;
 
 const notesSchema = new Schema({
-  title: { type: String, required: true },
+  title: { type: String, required: false },
   description: { type: String },
 });
 
