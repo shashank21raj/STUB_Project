db79853551f36c39b820ed618b55ba4241063699
Shashank <rajshashank2104@gmail.com>
2024-03-11 00:04:09 +0530

    Added Routes

--- stub_app/src/App.js
+++ stub_app/src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import axios from "axios";
+
+import Homepage from "./pages/homepage";
+
+axios.defaults.baseURL = "http://localhost:5000/";
+axios.defaults.withCredentials = true;
 
 function App() {
-  return <div className="text-3xl">This is react App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+    </Routes>
+  );
 }
 
 export default App;
