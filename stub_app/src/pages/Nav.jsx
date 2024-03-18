e4c708f2d9bf6cb7f3c17b9d5f3b83208f871f17
Shashank <rajshashank2104@gmail.com>
2024-03-19 02:27:57 +0530

    added Profile Link

--- stub_app/src/pages/Nav.jsx
+++ stub_app/src/pages/Nav.jsx
@@ -1,12 +1,22 @@
-import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { logout } from "../redux/reducer/userSlice";
 export default function Navbar() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const logoutButton = () => {
+    dispatch(logout());
+    navigate("/");
+  };
   return (
     <header className="flex justify-between border-b-2 bg-slate-950 text-white border-slate-900">
-      <Link to={"/"} className="text-4xl my-4 mx-12  ">
-        Student Hub
-      </Link>
+      <div className=" flex justify-center items-center">
+        <Link to={"/"} className="text-4xl my-2 mx-12  ">
+          Student Hub
+        </Link>
+      </div>
       <div className=" flex">
-        <div className="flex">
+        <div className="flex justify-center items-center">
           <Link
             to={"/career"}
             className="m-2 text-3xl  hover:bg-slate-900 rounded-2xl p-2 "
@@ -25,13 +35,19 @@ export default function Navbar() {
           >
             Docs
           </Link>
+          <Link
+            to={"/profile"}
+            className="mx-2 text-3xl my-2 hover:bg-slate-900 rounded-2xl p-2"
+          >
+            profile
+          </Link>
         </div>
-        <Link
-          to={"/profile"}
+        <div
           className="my-2 mx-12 text-3xl hover:bg-slate-900 rounded-2xl p-2"
+          onClick={(e) => logoutButton()}
         >
-          Profile
-        </Link>
+          Logout
+        </div>
       </div>
     </header>
   );
