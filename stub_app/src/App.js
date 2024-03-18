12ad8d2fef8323ddefc85dd2c1249be80af2fbd6
Shashank <rajshashank2104@gmail.com>
2024-03-19 02:31:11 +0530

    added Signin and SignUp route

--- stub_app/src/App.js
+++ stub_app/src/App.js
@@ -1,21 +1,40 @@
 import "./App.css";
-import { Route,  Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState } from "react";
+import { useSelector } from "react-redux";
 import axios from "axios";
 
 import Homepage from "./pages/homepage";
 import Navbar from "./pages/Nav";
+import SignIn from "./pages/signin";
+import SignUp from "./pages/signup";
+import ProfilePage from "./pages/profile";
 
 axios.defaults.baseURL = "http://localhost:5000/";
 axios.defaults.withCredentials = true;
 
 function App() {
+  const { currentUser } = useSelector((state) => state.user);
+  const [forSignin, setForSignin] = useState(false);
+  console.log("This is the current state ", currentUser);
   return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-      </Routes>
-    </>
+    <BrowserRouter>
+      <>
+        {currentUser ? (
+          <>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/profile" element={<ProfilePage />} />
+            </Routes>
+          </>
+        ) : forSignin ? (
+          <SignIn setForSignin={setForSignin} />
+        ) : (
+          <SignUp setForSignin={setForSignin} />
+        )}
+      </>
+    </BrowserRouter>
   );
 }
 
